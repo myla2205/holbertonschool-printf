d461aa13ffe8bdbbc1a7a855a620c9676555fefd
Rene Alsina <renealsina13@gmail.com>
2024-07-19 10:12:32 -0400

    combind it with cleanup

--- _printf.c
+++ _printf.c
@@ -1,6 +1,18 @@
 #include "main.h"
-
+void cleanup(va_list args, buffer_t *output);
 int _printf(const char *format, ...);
+
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
 
 /**
  * _printf - Outputs a formatted string.
@@ -22,4 +34,5 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	return (0);
+
 }
