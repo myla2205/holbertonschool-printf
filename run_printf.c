174c9b39d219ec4af64a58932f1aae63cd96813b
Rene Alsina <renealsina13@gmail.com>
2024-07-19 10:57:15 -0400

    added cleanup

--- run_printf.c
+++ run_printf.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
+void cleanup(va_list args, buffer_t *output);
 int run_printf(const char *format, va_list args, buffer_t *output);
+
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
 
 /**
  * run_printf - Reads through the format strings for _printf.
