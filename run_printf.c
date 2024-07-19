f3c2e37a72e065290d26fd858bbec9ad7279b4fa
Rene Alsina <renealsina13@gmail.com>
2024-07-19 10:40:43 -0400

    change misspelled

--- run_printf.c
+++ run_printf.c
@@ -47,7 +47,7 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 		ret += _memcpy(output, (format + i), 1);
 		i += (len != 0) ? 1 : 0;
 	}
-	cleanuo(args, output);
+	cleanup(args, output);
 	return (ret);
 
 }
