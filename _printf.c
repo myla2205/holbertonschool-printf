8dec14b5be484991ad492e4b3903b6d73d14a5bb
Rene Alsina <renealsina13@gmail.com>
2024-07-19 10:57:41 -0400

    remove cleanup

--- _printf.c
+++ _printf.c
@@ -1,18 +1,6 @@
 #include "main.h"
-void cleanup(va_list args, buffer_t *output);
-int _printf(const char *format, ...);
 
-/**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
-void cleanup(va_list args, buffer_t *output)
-{
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
+int _printf(const char *format, ...);
 
 /**
  * _printf - Outputs a formatted string.
